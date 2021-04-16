02c629dcccab46066be99ea21c5e1c7e48a17a6b
Chirag <chirag4798@gmail.com>
2021-04-16 23:07:08 +0530

    New Blog link

--- main.py
+++ main.py
@@ -109,4 +109,4 @@ def download():
     return send_file(buf_byt, mimetype="text/csv", as_attachment=True, attachment_filename="predictions.csv")
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=3000)
+    app.run(debug=True, host="127.0.0.1", port=3000)
